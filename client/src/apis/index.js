eda6f89e811c62515df2405ee02ecb69375f319e
sreerag <msratofficial@gmail.com>
2022-12-30 07:15:23 +0530

    fix:remove unwanted api base from index.js

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -4,28 +4,19 @@ export const api = axios.create({
     baseURL: 'http://localhost:5000/',
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json; charset=utf-8",
-    },
-
-
-  });
-
-export const api1 = axios.create({
-    baseURL: 'http://localhost:5000/',
-    headers: {
-        'Content-Type': `multipart/form-data`,
+      "Content-Type": "application/json; charset=utf-8 multipart/form-data" 
     },
 
 
   });
 
 
-export const editBlog=(id,data)=> api1.patch(`/blog/edit/${id}`,data);
-export const createBlog=(data)=>api1.post("/blog/create",data);
+
+export const editBlog=(id,data)=> api.patch(`/blog/edit/${id}`,data);
+export const createBlog=(data)=>api.post("/blog/create",data);
 
 export const setHead=(token)=>{
   api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  api1.defaults.headers.common['Authorization'] = `Bearer ${token}`; 
   localStorage.setItem('accessToken',token);
 }
 
@@ -38,7 +29,6 @@ export const registerUser=(data)=> api.post("/user/auth/register",data);
 export const loginUser=(data)=> api.post("/user/auth/login",data,{withCredentials:true});
 export const getUser=()=>api.get("/user");
 export const verifyToken=()=>api.get("/user/auth/get-accessToken",{withCredentials:true});
-export const verifyToken1=()=>api1.get("/user/auth/get-accessToken",{withCredentials:true});
 export const logoutUser=()=>api.get("/user/auth/logout",{withCredentials:true});
 
 
@@ -52,24 +42,6 @@ api.interceptors.response.use(res=>res,async error=>{
     if(response.status===200){
       setHead(response.data.accessToken);
       return api(error.config);
-    }
-  }
-
-  refresh=false;
-  return error;
-})
-
-
-refresh=false;
-
-
-api1.interceptors.response.use(res=>res,async error=>{
-  if(error.response.status===401 && !refresh){
-    refresh=true;
-    const response=await verifyToken1();
-    if(response.status===200){
-      setHead(response.data.accessToken);
-      return api1(error.config);
     }
   }
 
